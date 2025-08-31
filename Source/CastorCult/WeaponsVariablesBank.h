e5fb74109f67509b6d99397380e3f92b7e921d85
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-08-31 16:33:17 +0200

    Create WeaponsVariablesBank source files
    - Create and compile source files for WeaponVariablesBank DataTable.
    - Create a sample DataTable and export .json from it.

---
+++ Source/CastorCult/WeaponsVariablesBank.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "WeaponsVariablesBank.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWeaponsVariablesBankInfo
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Rarity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Type;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Properties;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Requirements;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Mods;
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponVariablesBank : public FTableRowBase
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Rarity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Type;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Properties;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Requirements;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+	TArray<FString> Mods;
+};
+
+UCLASS()
+class CASTORCULT_API UWeaponsVariablesBank : public UDataTable
+{
+	GENERATED_BODY()
+};
